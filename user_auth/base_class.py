bd242996668f23ea6461bb6394824dcf61868133
Chibuzo Kelechi <narutokelechi@gmail.com>
2024-03-03 16:27:24 +0100

    Fix db connection, add Alembic

--- user_auth/base_class.py
+++ user_auth/base_class.py
@@ -1,25 +1,22 @@
 from typing import Any
-from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy.orm import Mapped, as_declarative
 
-@declarative_base()
+# Base = declarative_base()
+
+# class User(Base):
+#     id: Mapped[int] = Column(Integer, primary_key=True)
+
+#     @declared_attr
+#     def __tablename__(cls) -> str:
+#         return cls.__name__.lower()
+
+@as_declarative()
 class Base:
     id: Any
     __name__: str
-    
-    @declared_attr # type: ignore
+
+    @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
-    
-# from typing import Any
-# from sqlalchemy.ext.declarative import declared_attr, declarative_base
-# # from sqlalchemy.orm import declarative_base
-
-# Base = declarative_base()
-# class User(Base):
-#     id: Any
-#     __name__: str
-    
-#     @declared_attr # type: ignore
-#     def __tablename__(cls) -> str:
-#         return cls.__name__.lower()
--- user_auth/main.py
+++ user_auth/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 from config import settings
 from session import engine
-from base_class import User
+from base_class import Base
 
-def create_table():
-    User.metadata.create_all(bind=engine)
+def create_table(): 
+    Base.metadata.create_all(bind=engine)
     
 def start_app():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     create_table()
-    return app
+    return app 
 
 app = start_app()
 
