f81d252f8307ab9b9c59485f743fdb02cc4da38c
Chibuzo Kelechi <narutokelechi@gmail.com>
2024-03-04 11:33:41 +0100

    News scraper v0

--- news_api/api.py
+++ news_api/api.py
@@ -1,14 +1,48 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from bs4 import BeautifulSoup
-# import schedule
+import requests
 
-news_app = FastAPI() 
 
-@news_app.get('/')
+news_api = FastAPI() 
+
+class WebScraper:
+    def __init__(self, url):
+        self.url = url
+        self.soup = None
+    
+    def get_page(self):
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            
+            self.soup = BeautifulSoup(response.content, 'lxml')
+            
+        except requests.exceptions.RequestException as e:
+            print(f'Error in scraping: {e}')
+                    
+    def find_element(self, tag, class_selector):
+        if not self.soup:
+            self.get_page()
+            
+        return self.soup.select(f'{tag}', class_=f'{class_selector}')
+    
+    def extract_text(self, elements, attributes=None):
+        if not elements:
+            return []
+        
+        if attributes:
+            return [element[attributes] for element in elements]
+        else:
+             return [element.get_text(strip=True) for element in elements]
+        
+    
+
+
+@news_api.get('/')
 async def main():
     return 'Testing the API'
 
-@news_app.get('/cryptofxnews')
+@news_api.get('/cryptofxnews')
 async def fxnews():
     return 'Crypto/Forex news endpoint'
