738e7ea542c41ff2a1361dc07aa028863f7cb9ff
Chibuzo Kelechi <narutokelechi@gmail.com>
2024-03-06 10:59:00 +0100

    Simple template

--- file_api/file_api.py
+++ file_api/file_api.py
@@ -1,12 +1,51 @@
 from fastapi import FastAPI, UploadFile, File
-import shutil
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Annotated
+
+
+class FileMaterial(BaseModel):
+    name: str
+    file_type: str
+    size: str
 
 
 file_app = FastAPI()
 
-@file_app.post('/files')
-async def get_file(file: bytes = File(...)):
-    content = file.decode('utf-8')
-    lines = content.split('\n')
+@file_app.get('/')
+async def check_home():
+    return 'Status: Active'
+
+
+@file_app.post('/files/')
+async def get_file(files: Annotated[list[bytes], File(description='Multi file as bytes')]):
     
-    return {'content': lines}
+    return {'content_length': [len(file) for file in files]}
+
+
+@file_app.post('/upload/')
+async def upload_file(
+    files: Annotated[
+        list[UploadFile], File(description='Multi-file upload')
+    ]
+):
+    # return {'filename': files.content_type}
+    return {'files': [file.filename for file in files]}
+
+
+@file_app.get('/html')
+async def main():
+    content = """
+     <body>
+        <form action="/files/" enctype="multipart/form-data" method="post">
+        <input name="files" type="file" multiple>
+        <input type="submit">
+        </form>
+        <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+        <input name="files" type="file" multiple>
+        <input type="submit">
+        </form>
+     </body>
+    """
+    
+    return HTMLResponse(content=content)
